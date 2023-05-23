ce43bc2755f52e8c921680d376edb22546926dc8
Norbert Melzer <timmelzer@gmail.com>
2023-05-23 18:40:14 +0200

    refactor: extract packages into own module

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
   outputs = {parts, ...} @ inputs:
     parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+      imports = [./packages];
 
       perSystem = {
-        self,
         inputs',
         self',
         pkgs,
@@ -15,86 +15,12 @@
       }: {
         formatter = pkgs.alejandra;
 
-        packages = let
-          version = "1.04pre";
-          tl = pkgs.texlive.combine {
-            inherit
-              (pkgs.texlive)
-              scheme-minimal
-              collection-luatex
-              babel-english
-              caption
-              dtxdescribe
-              etoolbox
-              fancyvrb
-              geometry
-              graphics
-              hypdoc
-              hyperref
-              infwarerr
-              kvoptions
-              latex-bin
-              microtype
-              newfloat
-              pdftexcmds
-              pict2e
-              tools
-              translations
-              xcolor
-              xetex
-              xstring
-              ;
-          };
-          inherit (pkgs) stdenvNoCC;
-          builder = engine:
-            stdenvNoCC.mkDerivation {
-              pname = "listllbls";
-              version = "${version}-${engine}";
-
-              src = "${self}";
-
-              nativeBuildInputs = [tl];
-
-              makeFlags = ["PREFIX=$(out)" "LATEX=${engine}" "SUDO="];
-
-              configurePhase = ''
-                export TEMP=$(pwd)
-                export HOME=$(pwd)
-              '';
-            };
-        in {
-          inherit tl;
-
-          xelatex = builder "xelatex";
-          lualatex = builder "lualatex";
-          pdflatex = builder "pdflatex";
-
-          zip = stdenvNoCC.mkDerivation {
-            pname = "listlbls";
-            inherit version;
-
-            src = "${self}";
-
-            nativeBuildInputs = [pkgs.zip tl];
-
-            makeFlags = ["zip" "VERS=$(version)"];
-
-            installPhase = ''
-              mkdir $out
-              install --mode 444 $pname-$version.zip $out/$pname-$version.zip
-            '';
+        devShells.default = pkgs.mkShellNoCC {
+          packages = builtins.attrValues {
+            inherit (pkgs) gnumake;
+            inherit (self'.packages) texCombined;
           };
         };
-
-        devShells.default = let
-          inherit (self'.packages) tl;
-        in
-          pkgs.mkShellNoCC {
-            packages = builtins.attrValues {
-              inherit (pkgs) gnumake;
-              inherit tl;
-            };
-          };
       };
     };
 }
